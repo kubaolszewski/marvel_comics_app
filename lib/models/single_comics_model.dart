3b75274d03dc52ec29cb799bb2c521a820cce7b7
Jakub Olszewski <kuba.olszewski.0704@icloud.com>
2023-05-10 18:41:23 +0200

    Add "authors" property to comics model

--- lib/models/single_comics_model.dart
+++ lib/models/single_comics_model.dart
@@ -2,10 +2,12 @@ class SingleComicsModel {
   SingleComicsModel({
     required this.title,
     required this.description,
+    required this.authors,
     required this.imagePath,
   });
 
   final String title;
   final String description;
+  final List<String> authors;
   final String imagePath;
 }
