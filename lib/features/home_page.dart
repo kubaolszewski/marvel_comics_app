274b10466bf65f756f73ca07c2543d03527820e9
Jakub Olszewski <kuba.olszewski.0704@icloud.com>
2023-05-10 17:40:06 +0200

    Implement simple navigation bar

--- lib/features/home_page.dart
+++ lib/features/home_page.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-   const HomePage({super.key});
- 
-   @override
-   Widget build(BuildContext context) {
-     return Container();
-   }
- }
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.red,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              size: 32,
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search,
+              size: 32,
+            ),
+            label: '',
+          ),
+        ],
+      ),
+    );
+  }
+}
