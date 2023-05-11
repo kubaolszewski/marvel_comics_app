1a2294ef9bb35927209301290aaa4d17cec2a22e
Jakub Olszewski <kuba.olszewski.0704@icloud.com>
2023-05-11 21:43:17 +0200

    Redesign home page

--- lib/app/features/home_page.dart
+++ lib/app/features/home_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:marvel_comics_app/app/features/cubit/home_page_cubit.dart';
 import 'package:marvel_comics_app/app/features/list_page/list_page_content.dart';
-import 'package:marvel_comics_app/app/features/search_page/search_page_content.dart';
 import 'package:marvel_comics_app/data/remote_data_source.dart';
 import 'package:marvel_comics_app/repositories/comics_repository.dart';
 
@@ -23,11 +22,20 @@ class HomePage extends StatelessWidget {
             appBar: AppBar(
               actions: [
                 IconButton(
-                  icon: const Icon(Icons.search),
+                  icon: const Icon(
+                    Icons.search,
+                    color: Colors.red,
+                  ),
                   onPressed: () {},
                 ),
               ],
-              leading: const Icon(Icons.arrow_back),
+              leading: IconButton(
+                icon: const Icon(
+                  Icons.arrow_back,
+                  color: Colors.red,
+                ),
+                onPressed: () {},
+              ),
               title: Text(
                 'Marvel Comics',
                 style: GoogleFonts.poppins(
@@ -36,56 +44,11 @@ class HomePage extends StatelessWidget {
                 ),
               ),
             ),
-            body: Builder(
-              builder: (context) {
-                if (state.pageIndex == 0) {
-                  return const ListPage();
-                }
-                return const SearchPage();
-              },
-            ),
-            bottomNavigationBar:
-                _CustomBottomNavigationBar(pageIndex: state.pageIndex),
+            body: const ListPage(),
+            
           );
         },
       ),
-    );
-  }
-}
-
-class _CustomBottomNavigationBar extends StatelessWidget {
-  const _CustomBottomNavigationBar({
-    required this.pageIndex,
-  });
-
-  final int pageIndex;
-
-  @override
-  Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      backgroundColor: Colors.red,
-      selectedItemColor: Colors.white,
-      unselectedItemColor: Colors.black,
-      currentIndex: pageIndex,
-      onTap: (newPageIndex) {
-        context.read<HomePageCubit>().changeIndex(newPageIndex);
-      },
-      items: const [
-        BottomNavigationBarItem(
-          icon: Icon(
-            Icons.home,
-            size: 32,
-          ),
-          label: '',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(
-            Icons.search,
-            size: 32,
-          ),
-          label: '',
-        ),
-      ],
     );
   }
 }
