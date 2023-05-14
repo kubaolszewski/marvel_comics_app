ae94850e05287c8e37429def7de5f4d4677de640
Jakub Olszewski <kuba.olszewski.0704@icloud.com>
2023-05-14 16:08:35 +0200

    Remove unnecessary home page cubit

--- lib/app/features/home_page.dart
+++ lib/app/features/home_page.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:marvel_comics_app/app/features/cubit/home_page_cubit.dart';
 import 'package:marvel_comics_app/app/features/list_page/cubit/list_page_cubit.dart';
 import 'package:marvel_comics_app/app/features/list_page/list_page_content.dart';
 import 'package:marvel_comics_app/app/features/search_page/cubit/search_page_cubit.dart';
 import 'package:marvel_comics_app/app/features/search_page/search_page_content.dart';
-import 'package:marvel_comics_app/data/remote_data_source.dart';
-import 'package:marvel_comics_app/repositories/comics_repository.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({
@@ -16,51 +13,41 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => HomePageCubit(
-        ComicsRepository(
-          comicsRemoteDataSource: ComicsRemoteDataSource(),
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 8.0,
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.search,
+              color: Colors.red,
+            ),
+            onPressed: () async {
+              await showSearch(
+                  context: context,
+                  delegate:
+                      ComicSearchPage(searchPageCubit: SearchPageCubit()));
+            },
+          ),
+        ],
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.red,
+          ),
+          onPressed: () {},
+        ),
+        title: Text(
+          'Marvel Comics',
+          style: GoogleFonts.poppins(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
-      child: BlocBuilder<HomePageCubit, HomePageState>(
-        builder: (context, state) {
-          return Scaffold(
-              appBar: AppBar(
-                elevation: 8.0,
-                actions: [
-                  IconButton(
-                    icon: const Icon(
-                      Icons.search,
-                      color: Colors.red,
-                    ),
-                    onPressed: () async {
-                      await showSearch(
-                          context: context,
-                          delegate: ComicSearchPage(
-                              searchPageCubit: SearchPageCubit()));
-                    },
-                  ),
-                ],
-                leading: IconButton(
-                  icon: const Icon(
-                    Icons.arrow_back,
-                    color: Colors.red,
-                  ),
-                  onPressed: () {},
-                ),
-                title: Text(
-                  'Marvel Comics',
-                  style: GoogleFonts.poppins(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              body: BlocProvider(
-                create: (context) => ListPageCubit()..fetchComics(),
-                child: const ListPage(),
-              ));
-        },
+      body: BlocProvider(
+        create: (context) => ListPageCubit()..fetchComics(),
+        child: const ListPage(),
       ),
     );
   }
